6079cb321a2bf55207f4027c425ab18b2fb750e1
Uwe Gerdes <entwicklung@uwegerdes.de>
2017-08-18 21:46:46 +0200

    replaced index.js with promises.js, version 0.5.0

--- index.js
+++ index.js
@@ -1,256 +1,296 @@
-/*
- * Laden von Styledaten von zwei HTML-Seiten vom Browser
- * und Vergleich von HTML-Styles für Regressions- und Back-to-Back-Tests
+/**
+ * load pages in phantomjs or slimerjs, grab elements, use viewports, prepare results for server
  *
- * node compare-layouts.js [configfile] [-v] [-r]
+ * node index.js [configfile] [-v] [-r]
  *
- * configfile: config/name.js erwartet, Beispiele in ./config
+ * configfile: config/[name].js
  * -v: verbose
  * -r: force reload / ignore cache
  *
  * (c) Uwe Gerdes, entwicklung@uwegerdes.de
  */
+
 'use strict';
 
-var fs = require('fs'),
-	exec = require('child_process').exec,
-	path = require('path'),
-	styleTree = require('./bin/style-tree.js');
+var exec = require('child_process').exec,
+  fs = require('fs'),
+  path = require('path'),
+  styleTree = require('./bin/style-tree.js');
 
 var configFile = 'config/default.js',
-	config = null;
+  config = null;
 
-if (process.argv[2]) {
-	configFile = process.argv[2];
-}
 config = require('./' + configFile);
-
-var reload = process.argv.indexOf('-r') > -1;
-if (reload) {
-	console.log('forced reload');
-}
+var resultsDir = './results';
+var destDir = path.join(resultsDir, config.destDir);
 
 var verbose = process.argv.indexOf('-v') > -1;
 
-var resultsDir = './results';
-var destDir = path.join(resultsDir, config.destDir);
-var pages = config.pages;
-var compares = config.compares;
-var pagesLoaded = 0;
-//var pagesExpected = [];
-//var pagesLoaded = [];
-var results = {};
-
-if (!fs.existsSync(resultsDir)) {
-	fs.mkdirSync(resultsDir);
+function getFile(filename) {
+  console.log('reading: ' + filename);
+  return new Promise(
+    function(resolve, reject) {
+      fs.readFile(filename, function(error, data) {
+        if (error) {
+          reject(Error(error.code));
+        }
+        resolve(data.toString());
+      });
+    }
+  );
 }
-fs.stat(destDir, function(err, stats) {
-	if (!stats) {
-		fs.mkdir(destDir,
-			function (err, data) {
-				if (err) { throw err; }
-				if (data) {console.log(data); }
-				console.log('directory "' + destDir + '" created');
-				load();
-			}
-		);
-		console.log('creating directory "' + destDir + '"');
-	} else {
-		if (!stats.isDirectory()) {
-			console.log(destDir + ' exists but is not a directory');
-		} else {
-			load();
-			comparePages(); // cache-cache compare (for developement)
-		}
-	}
+
+function getJSON(filename) {
+  return getFile(filename).then(JSON.parse);
+}
+
+function loadPage(pageKey) {
+  var page = config.pages[pageKey];
+  if (page.cache && chkCacheFile(path.join(destDir, pageKey, Object.keys(config.viewports)[0], 'page.png'))) {
+    console.log('cached page  ' + pageKey + ': ' + page.url);
+    return new Promise(function(resolve, reject) {
+      resolve({'pageKey': pageKey, 'status': 'cached'});
+    });
+  }
+  return new Promise(function(resolve, reject) {
+    var args = [
+      './bin/load-page-styles.js',
+      '--configFile="' + configFile + '"',
+      '--pageKey="' + pageKey + '"'];
+    var cmd = 'casperjs';
+    if (page.engine) {
+      args.unshift('--engine="' + page.engine + '"');
+      if (page.engine == 'slimerjs') {
+        if (process.platform == "linux") {
+//        cmd = 'xvfb-run -a -e /dev/stdout casperjs';
+          cmd = 'xvfb-run -a casperjs';
+        }
+      }
+    }
+    console.log('loading page ' + pageKey + ': ' + page.url);
+    var loader = exec(cmd + ' ' + args.join(' '), function(error, stdout, stderr) {
+      if (verbose || stdout.indexOf('element not found') > -1) {
+        console.log(pageKey + ': ' + stdout.trim());
+      }
+    });
+    loader.stderr.on('data', function(stderr) {
+      console.log(pageKey + ' stderr: ' + stderr.trim());
+    });
+    loader.on('error', function(error) {
+      console.log(pageKey + ' error: ' + error);
+    });
+    loader.on('close', function(error) {
+      console.log('loaded page ' + pageKey + ': ' + page.url);
+      if (error > 0) {
+        reject({'pageKey': pageKey, 'status': 'error', 'exitcode': error});
+      } else {
+        resolve({'pageKey': pageKey, 'status': 'loaded'});
+      }
+    });
+  });
+}
+
+function makeCompareDir(compareKey) {
+  if (!fs.existsSync(path.join(destDir,  safeFilename(compareKey)))) {
+    fs.mkdir(path.join(destDir,  safeFilename(compareKey)),function(error) {
+      if (error) {
+        console.log('makeCompareDir ' + compareKey + ' error: ' + error);
+      }
+      return new Promise(function(resolve, reject) {
+        resolve(compareKey);
+      });
+    });
+  } else {
+    return new Promise(function(resolve, reject) {
+      resolve(compareKey);
+    });
+  }
+}
+
+function compareData(result) {
+  return new Promise(function(resolve, reject) {
+    var compareKey = result.compareKey;
+    var viewport = result.viewport;
+    var compare = config.compares[compareKey];
+    var page1 = config.pages[compare.page1];
+    var page2 = config.pages[compare.page2];
+    result.name = compareKey;
+    result.viewport = viewport;
+    result.page1 = page1;
+    result.page2 = page2;
+    result.subdir1 = compare.page1;
+    result.subdir2 = compare.page2;
+    result.selector1 = compare.selector1 ? compare.selector1 : page1.selector;
+    result.selector2 = compare.selector2 ? compare.selector2 : page2.selector;
+    result.baseFilename1 = path.join(destDir,  compare.page1,  viewport,  safeFilename(result.selector1));
+    result.baseFilename2 = path.join(destDir,  compare.page2,  viewport,  safeFilename(result.selector2));
+    result.htmlFilename1 = [destDir, compare.page1, viewport, safeFilename(result.selector1)].join('/');
+    result.htmlFilename2 = [destDir, compare.page2, viewport, safeFilename(result.selector2)].join('/');
+    result.exists1 = chkCacheFile(result.baseFilename1 + '.json');
+    result.exists2 = chkCacheFile(result.baseFilename2 + '.json');
+    result.success = true;
+    result.path = path.join(destDir,  safeFilename(compareKey),  viewport);
+    result.compareFilename = result.path + '_compare.png';
+    result.compositeFilename = result.path + '_composite.png';
+    result.jsonFilename = result.path + '.json';
+    result.htmlFilename = result.path + '.html';
+    resolve(result);
+  });
+}
+
+function compareImages(result) {
+  return new Promise(function(resolve, reject) {
+    exec('compare -metric AE "' + result.baseFilename1 + '.png" "' + result.baseFilename2 + '.png" ' + result.compareFilename,
+      function (error, stdout, stderr) {
+        //if (verbose) { logExecResult('compare', null, stdout, stderr.replace(/ @.+/, '').replace(/^0$/, '')); }
+        if (stderr == '0') {
+          if (verbose) { console.log(result.compareFilename + ' saved'); }
+        } else {
+          result.compareFilename = '';
+          result.success = false;
+        }
+        result.compareImagesStderr = stderr;
+        resolve(result);
+      }
+    );
+  });
+}
+
+function compositeImages(result) {
+  return new Promise(function(resolve, reject) {
+    exec('composite -compose difference "' + result.baseFilename1 + '.png" "' + result.baseFilename2 + '.png" ' + result.compositeFilename,
+      function (error, stdout, stderr) {
+        //logExecResult('composite', null, stdout, stderr.replace(/ @.+/, ''));
+        if (stderr.length === 0) {
+          if (verbose) { console.log(result.compositeFilename + ' saved'); }
+        } else {
+          result.compositeFilename = '';
+          result.success = false;
+        }
+        result.compositeImagesStderr = stderr;
+        resolve(result);
+      }
+    );
+  });
+}
+
+function compareStyleTree(comp) {
+  var compareKey = comp.compareKey;
+  var viewport = comp.viewport;
+  var compare = config.compares[compareKey];
+  var page1 = config.pages[compare.page1];
+  var selector1 = compare.selector1 ? compare.selector1 : page1.selector;
+  var page2 = config.pages[compare.page2];
+  var selector2 = compare.selector2 ? compare.selector2 : page2.selector;
+  var styleTree1 = styleTree(JSON.parse(fs.readFileSync(path.join(destDir,  compare.page1,  viewport,  safeFilename(selector1) + '.json'))));
+  var styleTree2 = styleTree(JSON.parse(fs.readFileSync(path.join(destDir,  compare.page2,  viewport,  safeFilename(selector2) + '.json'))));
+  var compareResult = styleTree1.compareTo(styleTree2, compare.compare);
+  var jsonFilename = path.join(destDir,  safeFilename(compareKey),  viewport + '.json');
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(jsonFilename, JSON.stringify(compareResult, undefined, 4), function(error) {
+      if(error) {
+        console.log(jsonFilename + ' error: ' + error);
+        compare.error = error;
+        reject(compare);
+      }
+      console.log(jsonFilename + ' saved');
+      compare.jsonFilename = jsonFilename;
+      resolve(compare);
+    });
+  });
+}
+
+function saveResults(results) {
+  let output = {};
+  results.forEach(function (result) {
+    output[result.compareKey + '_' + result.viewport] = result;
+  });
+  fs.writeFile(path.join(destDir,  'index.json'), JSON.stringify(output, null, 4), function(error) {
+    if(error) {
+      return console.log(path.join(destDir,  'index.json') + ' error: ' + error);
+    }
+    console.log('result in: ' + path.join(destDir,  'index.json'));
+  });
+}
+
+var results = {};
+var compares = [];
+Object.keys(config.compares).forEach(function(compareKey) {
+  Object.keys(config.viewports).forEach(function(viewport) {
+    compares.push({'compareKey': compareKey, 'viewport': viewport});
+  });
+});
+var promise = new Promise((resolve, reject) => { resolve(path.basename(__filename)); });
+promise = promise.then(function(response) {
+  console.log("Starting:", response);
+  return response;
+}).then(function(response) {
+  var pageKeys = Object.keys(config.pages);
+  return Promise.all(
+    pageKeys.map(loadPage)
+  );
+}).then(function(pages) {
+  results.pages = pages;
+  return Promise.all(
+    compares.map(compareData)
+  );
+}).then(function(data) {
+  return Promise.all(
+    Object.keys(config.compares).map(makeCompareDir)
+  );
+}).then(function() {
+  return Promise.all(
+    compares.map(compareImages)
+  );
+}).then(function(compares) {
+  results.compares = compares;
+  return Promise.all(
+    compares.map(compositeImages)
+  );
+}).then(function(compares) {
+  results.compares = compares;
+  return Promise.all(
+    compares.map(compareStyleTree)
+  );
+});
+promise.then(function(compared) {
+  saveResults(results.compares);
+}).catch(function(error) {
+  console.error("Failed!", error);
 });
 
-// the working directory has changed to data directory
-function load() {
-	Object.keys(pages).forEach(function(pageKey) {
-		var page = pages[pageKey];
-		var selectorList;
-		if (page.selectorList) {
-			selectorList = page.selectorList;
-		} else {
-			selectorList = page.selector.split(/,/);
-		}
-		if (reload || !page.cache || !fs.existsSync(path.join(destDir,  pageKey))) {
-			console.log('loading: ' + pageKey);
-			loadPage(configFile, pageKey, page);
-		} else {
-			console.log('cached : ' + pageKey);
-			pagesLoaded++;
-		}
-	});
+function makePath(directoryPath) {
+  const directory = path.normalize(directoryPath);
+  return new Promise((resolve, reject) => {
+    fs.stat(directory, (error) => {
+      if (error) {
+        if (error.code === 'ENOENT') {
+          fs.mkdir(directory, (error) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(directory);
+            }
+          });
+        } else {
+          reject(error);
+        }
+      } else {
+        resolve(directory);
+      }
+    });
+  });
 }
 
-function loadPage(configFile, pageKey, page) {
-	page.loaded = false;
-	var args = ['./bin/load-page-styles.js',
-		'--configFile="' + configFile + '"',
-		'--pageKey="' + pageKey + '"'];
-	var cmd = 'casperjs';
-	if (page.engine) {
-		args.unshift('--engine="' + page.engine + '"');
-		if (page.engine == 'slimerjs') {
-			if (process.platform == "linux") {
-//				cmd = 'xvfb-run -a -e /dev/stdout casperjs';
-				cmd = 'xvfb-run -a casperjs';
-			}
-		}
-	}
-	var loader = exec(cmd + ' ' + args.join(' '),
-		function (error, stdout, stderr) {
-			logExecResult('loaded page ' + page.url, error, "", stderr);
-		}
-	);
-	loader.stdout.on('data', function(data) { if (verbose || data.indexOf('element not found') > -1) {console.log(pageKey + ': ' + data.trim());} });
-	loader.stderr.on('data', function(data) { console.log(pageKey + ' stderr: ' + data.trim()); });
-	loader.on('error', function(err) { console.log(pageKey + ' error: ' + err); });
-	loader.on('close', function(code) {
-		if (code > 0) {
-			console.log('load ' + page.url + ' exit: ' + code);
-		}
-		page.loaded = true;
-		pagesLoaded++;
-		comparePages();
-	});
-}
-
-var success = true;
-var pagesLoading = [];
-function comparePages() {
-	if (pagesLoaded < Object.keys(pages).length) {
-		return;
-	}
-	Object.keys(compares).forEach(function(key) {
-		console.log('compare ' + key);
-		if (!fs.existsSync(path.join(destDir,  safeFilename(key)))) {
-			fs.mkdirSync(path.join(destDir,  safeFilename(key)));
-		}
-		var compare = compares[key];
-		var page1 = pages[compare.page1];
-		var page2 = pages[compare.page2];
-		if (page1 === null || page1 === undefined) {
-			console.log('ERROR in configfile: compage selector page1 "' + compare.page1 + '" not found');
-			return;
-		}
-		if (page2 === null || page2 === undefined) {
-			console.log('ERROR in configfile: compage selector page2 "' + compare.page2 + '" not found');
-			return;
-		}
-		Object.keys(config.viewports).forEach(function(viewport) {
-			var result = {};
-			result.name = key;
-			result.viewport = viewport;
-			result.page1 = page1;
-			result.page2 = page2;
-			result.subdir1 = compare.page1;
-			result.subdir2 = compare.page2;
-			result.selector1 = compare.selector1 ? compare.selector1 : page1.selector;
-			result.selector2 = compare.selector2 ? compare.selector2 : page2.selector;
-			result.baseFilename1 = path.join(destDir,  compare.page1,  viewport,  safeFilename(result.selector1));
-			result.baseFilename2 = path.join(destDir,  compare.page2,  viewport,  safeFilename(result.selector2));
-			result.htmlFilename1 = destDir + '/' +  compare.page1 + '/' +  viewport + '/' +  safeFilename(result.selector1);
-			result.htmlFilename2 = destDir + '/' + compare.page2 + '/' +  viewport + '/' +  safeFilename(result.selector2);
-			result.exists1 = chkCacheFile(result.baseFilename1 + '.json');
-			result.exists2 = chkCacheFile(result.baseFilename2 + '.json');
-			result.success = false;
-			if ((page1.cache || page1.loaded) && result.exists1 &&
-				(page2.cache || page2.loaded) && result.exists2) {
-				var resultPath = path.join(destDir,  safeFilename(key),  viewport);
-				pagesLoading.push(key);
-				result.success = true;
-				result.compareFilename = resultPath + '_compare.png';
-				result.compositeFilename = resultPath + '_composite.png';
-				result.jsonFilename = resultPath + '.json';
-				result.htmlFilename = resultPath + '.html';
-				exec('compare -metric AE "' + result.baseFilename1 + '.png" "' + result.baseFilename2 + '.png" ' + result.compareFilename,
-					function (error, stdout, stderr) {
-						if (verbose) { logExecResult('compare', null, stdout, stderr.replace(/ @.+/, '').replace(/^0$/, '')); }
-						if (stderr == '0') {
-							if (verbose) { console.log(result.compareFilename + ' saved'); }
-						} else {
-							result.success = false;
-							success = false;
-						}
-						result.compareImagesStderr = stderr;
-						exec('composite -compose difference "' + result.baseFilename1 + '.png" "' + result.baseFilename2 + '.png" ' + result.compositeFilename,
-							function (error, stdout, stderr) {
-								logExecResult('composite', null, stdout, stderr.replace(/ @.+/, ''));
-								if (stderr.length === 0) {
-									if (verbose) { console.log(result.compositeFilename + ' saved'); }
-								} else {
-									result.compositeFilename = '';
-									result.success = false;
-									success = false;
-								}
-								compareResults(compare, key, viewport);
-							}
-						);
-					}
-				);
-			} else {
-				console.log('loaded pages not found');
-			}
-			results[ key + '_' + viewport ] = result;
-		});
-	});
-}
-
-function compareResults(compare, name, viewport) {
-	var page1 = pages[compare.page1];
-	var selector1 = compare.selector1 ? compare.selector1 : page1.selector;
-	var page2 = pages[compare.page2];
-	var selector2 = compare.selector2 ? compare.selector2 : page2.selector;
-	var result = true;
-	if (chkCacheFile(path.join(destDir,  compare.page1,  viewport,  safeFilename(selector1) + '.json')) &&
-		chkCacheFile(path.join(destDir,  compare.page2,  viewport,  safeFilename(selector2) + '.json'))) {
-		var styleTree1 = styleTree(JSON.parse(fs.readFileSync(path.join(destDir,  compare.page1,  viewport,  safeFilename(selector1) + '.json'))));
-		var styleTree2 = styleTree(JSON.parse(fs.readFileSync(path.join(destDir,  compare.page2,  viewport,  safeFilename(selector2) + '.json'))));
-		var compareResult = styleTree1.compareTo(styleTree2, compare.compare);
-		var jsonFilename = path.join(destDir,  safeFilename(name),  viewport + '.json');
-		fs.writeFile(jsonFilename, JSON.stringify(compareResult, undefined, 4), function(err) {
-			if(err) {
-				return console.log(jsonFilename + ' error: ' + err);
-			}
-			console.log(jsonFilename + ' saved');
-			pagesLoading.splice(pagesLoading.indexOf(compare), 1);
-			if (pagesLoading.length === 0) {
-				saveResults(compare, results, success);
-			}
-		});
-	} else {
-		result = false;
-	}
-	return result;
-}
-
-function saveResults(compare, results, success) {
-	fs.writeFile(path.join(destDir,  'index.json'), JSON.stringify(results, null, 4), function(err) {
-		if(err) {
-			return console.log(path.join(destDir,  'index.json') + ' error: ' + err);
-		}
-		console.log((success ? "SUCCESS" : "FAIL") + ' - result in: ' + path.join(destDir,  'index.json'));
-	});
-}
-
-function logExecResult(msgStart, error, stdout, stderr) {
-	if (stdout.length > 0) { console.log(msgStart + ' stdout: ' + stdout.trim()); }
-	if (stderr.length > 0) { console.log(msgStart + ' stderr: ' + stderr.trim()); }
-	if (error !== null)	{ console.log(msgStart + ' error:\n' + JSON.stringify(error, undefined, 4)); }
-}
-
-function chkCacheFile(filename) {
-	try {
-		return fs.lstatSync(filename).isFile();
-	} catch(err) {
-		// console.log('chk ' + filename + ' not found');
-	}
-	return false;
+function chkCacheFile(file) {
+  try {
+    return fs.lstatSync(file).isFile();
+  } catch(error) {
+    //console.log('chkCacheFile ' + file + ' not found');
+  }
+  return false;
 }
 
 function safeFilename(name) {
-	return name.replace(/[ .?#/:\(\)<>|\\]/g, "_").trim();
+  return name.replace(/[ .?#/:\(\)<>|\\]/g, "_").trim();
 }
