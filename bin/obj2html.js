10821c2f65a4aa316c729f03842396eec74e17e4
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-04-25 23:13:21 +0200

    updating jsDoc

--- bin/obj2html.js
+++ bin/obj2html.js
@@ -9,16 +9,22 @@
 const ind = '\t';
 
 module.exports = {
-  toHtml: (obj) => {
+  toHtml: (obj) => { // jscs:ignore jsDoc
     return toHTML(obj, ind);
   }
 };
 
+/**
+ * convert object to html using indentation
+ *
+ * @param {Object} obj - Object, Array or other value
+ * @param {String} indent - indentation for output - it accumulates
+ */
 function toHTML(obj, indent) {
   let html = '';
   if (type(obj) == 'Array') {
     html += indent + '<ol>\n';
-    obj.forEach((obj) => {
+    obj.forEach((obj) => { // jscs:ignore jsDoc
       html += indent + ind + '<li>\n';
       html += toHTML(obj, indent + ind + ind);
       html += indent + ind + '</li>\n';
@@ -26,7 +32,7 @@ function toHTML(obj, indent) {
     html += indent + '</ol>\n';
   } else if (type(obj) == 'Object') {
     html += indent + '<dl>\n';
-    Object.keys(obj).forEach((key) => {
+    Object.keys(obj).forEach((key) => { // jscs:ignore jsDoc
       html += indent + ind + '<dt class="' + key + '">' + key + '</dt>\n' + indent + ind +
         '<dd class="' + key + ' ' + type(obj[key]) + '">';
       if (type(obj) == 'Array') {
@@ -46,6 +52,11 @@ function toHTML(obj, indent) {
   return html;
 }
 
+/**
+ * get the type of obj
+ *
+ * @param {Object} obj - Object, Array or other value
+ */
 function type(obj) {
   return Object.prototype.toString.call(obj).replace(/\[object (.+)\]/, '$1');
 }
